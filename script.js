c45039d8c8f6ff7200ad47aa28bbeb94c1a9b2e0
briann39 <brianalvernas8@gmail.com>
2025-07-15 22:27:23 -0300

    Mejora en interfaz, se quito backgorunds dinamicos por iconos, mejora en el codigo

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const windDisplay = document.getElementById("wind");
 const iconDisplay = document.getElementById("weather-icon");
 
 const videoBackground = document.getElementById("video-background"); // Selección del video de fondo
-const sourceBackground = document.getElementById("source-background"); // Selección del video de fondo
+const sourceIcon = document.getElementById("weatherIcon"); // Selección del video de fondo
 
 // Elementos para la búsqueda
 const inputLocation = document.getElementById("inputLocation");
@@ -23,13 +23,70 @@ const errorMessage = document.getElementById("error-message");
 // Contenedor para las sugerencias de ciudades
 const suggestionsContainer = document.getElementById("city-suggestions");
 
+// Evento para autocompletar ciudades
+let debounceTimeout;
+
+// Mapeo de descripciones y fondos de video para los diferentes tipos de clima
+// Estos mapeos se utilizan para mostrar descripciones y cambiar el fondo de video según el
+const descriptionMap = {
+  Clear: "Despejado",
+  Clouds: "Nuboso",
+  Rain: "Lluvia",
+  Drizzle: "LLovizna",
+  Thunderstorm: "Tormenta",
+  Snow: "Nieve",
+  Mist: "Neblina",
+  Smoke: "Humo",
+  Haze: "Niebla",
+  Dust: "Polvo",
+  Fog: "Niebla",
+  Sand: "Arena",
+  Ash: "Ceniza",
+  Squall: "Chubasco",
+  Tornado: "Tornado",
+};
+const iconMap = {
+  Clear: "backgrounds/clear.svg",
+  Clouds: "backgrounds/clouds.svg",
+  Rain: "backgrounds/rainy-6.svg",
+  Drizzle: "backgrounds/rainy-4.svg",
+  Thunderstorm: "backgrounds/thunder.svg",
+  Snow: "backgrounds/snowy.svg",
+  Mist: "backgrounds/fog.svg",
+  Smoke: "backgrounds/fog.svg",
+  Haze: "backgrounds/haze.svg",
+  Dust: "backgrounds/fog.svg",
+  Fog: "backgrounds/fog.svg",
+  Sand: "backgrounds/haze.svg",
+  Ash: "backgrounds/fog.svg",
+  Squall: "backgrounds/wind.svg",
+  Tornado: "backgrounds/hurricane.svg",
+};
+
 window.onload = () => {
   geoLocation();
 }; // Llama a la función para obtener la ubicación del usuario al cargar la página
 
-// Evento para autocompletar ciudades
-let debounceTimeout;
+inputLocation.addEventListener("input", function () {
+  clearTimeout(debounceTimeout);
+  debounceTimeout = setTimeout(() => {
+    const query = inputLocation.value.trim();
+    fetchCities(query); // Llama a la función para buscar ciudades
+  }, 350); // Espera 500ms después de dejar de escribir
+}); // Evento para autocompletar ciudades al escribir en el campo de entrada
 
+// Ocultar sugerencias al perder foco
+inputLocation.addEventListener("blur", function () {
+  setTimeout(() => {
+    suggestionsContainer.innerHTML = "";
+  }, 100);
+});
+// Evento para el botón de búsqueda
+buttonSearch.addEventListener("click", searchWeather);
+
+// Función para buscar ciudades
+// Esta función toma una consulta de búsqueda y realiza una solicitud a la API de GeoDB para buscar ciudades
+// Luego, muestra las sugerencias de ciudades en el contenedor de sugerencias
 async function fetchCities(query) {
   // URL de la API para buscar ciudades
   // Esta URL utiliza la API de GeoDB para buscar ciudades por prefijo de nombre
@@ -72,98 +129,34 @@ async function fetchCities(query) {
   }
 }
 
-inputLocation.addEventListener("input", function () {
-  clearTimeout(debounceTimeout);
-  debounceTimeout = setTimeout(() => {
-    const query = inputLocation.value.trim();
-    fetchCities(query); // Llama a la función para buscar ciudades
-  }, 350); // Espera 500ms después de dejar de escribir
-});
+// Función para actualizar los datos del clima
+// Esta función toma una URL como parámetro para realizar la solicitud a la API
+async function actualitationData(url) {
+  // Realiza la solicitud a la API de OpenWeatherMap
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      if (response.status === 401) {
+        //Llamar a la función showError para mostrar un mensaje de error específico
+        showError(
+          "Porfavor contacta con un administrador de la web, hubo un error al conectar con la API"
+        ); // Muestra un mensaje de error específico
+      } else {
+        // Llamar a la función showError para mostrar un mensaje de error genérico
+        showError(
+          "No se pudo obtener el clima. Por favor, verifica la ciudad ingresada."
+        ); // Muestra un mensaje de error
+      }
+      return; // Sale de la función si la respuesta no es exitosa
+    }
 
-// Ocultar sugerencias al perder foco
-inputLocation.addEventListener("blur", function () {
-  setTimeout(() => {
-    suggestionsContainer.innerHTML = "";
-  }, 100);
-});
+    const data = await response.json(); // Procesa la respuesta JSON de la API
 
-// Mapeo de iconos de Font Awesome para los diferentes tipos de clima
-const iconMap = {
-  Clear: "fas fa-sun",
-  Clouds: "fas fa-cloud",
-  Rain: "fas fa-cloud-showers-heavy",
-  Drizzle: "fas fa-cloud-rain",
-  Thunderstorm: "fas fa-bolt",
-  Snow: "fas fa-snowflake",
-  Mist: "fas fa-smog",
-  Smoke: "fas fa-smog",
-  Haze: "fas fa-smog",
-  Dust: "fas fa-smog",
-  Fog: "fas fa-smog",
-  Sand: "fas fa-smog",
-  Ash: "fas fa-smog",
-  Squall: "fas fa-wind",
-  Tornado: "fas fa-poo-storm",
-};
-
-// Mapeo de descripciones y fondos de video para los diferentes tipos de clima
-// Estos mapeos se utilizan para mostrar descripciones y cambiar el fondo de video según el
-const descriptionMap = {
-  Clear: "Despejado",
-  Clouds: "Nuboso",
-  Rain: "Lluvia",
-  Drizzle: "LLovizna",
-  Thunderstorm: "Tormenta",
-  Snow: "Nieve",
-  Mist: "Neblina",
-  Smoke: "Humo",
-  Haze: "Niebla",
-  Dust: "Polvo",
-  Fog: "Niebla",
-  Sand: "Arena",
-  Ash: "Ceniza",
-  Squall: "Chubasco",
-  Tornado: "Tornado",
-};
-const backgroundMap = {
-  Clear: "backgrounds/clear.mp4",
-  Clouds: "backgrounds/clouds.mp4",
-  Rain: "backgrounds/rain.mp4",
-  Drizzle: "backgrounds/drizzle.mp4",
-  Thunderstorm: "backgrounds/thunderstorm.mp4",
-  Snow: "backgrounds/Snow/.mp4",
-  Mist: "backgrounds/Mist.mp4",
-  Smoke: "Humo",
-  Haze: "Niebla",
-  Dust: "Polvo",
-  Fog: "Niebla",
-  Sand: "Arena",
-  Ash: "Ceniza",
-  Squall: "Chubasco",
-  Tornado: "Tornado",
-};
-
-// Evento para el botón de búsqueda
-buttonSearch.addEventListener("click", searchWeather);
-
-// Función para buscar el clima
-function searchWeather(event) {
-  event.preventDefault(); // Evita el comportamiento por defecto del formulario
-  if (inputLocation.value === "") {
-    // Verifica si el campo de entrada está vacío
-    errorContainer.style.display = "flex"; // Muestra el contenedor de errores
-    errorMessage.textContent = "Por favor, ingresa una ciudad"; // Muestra un mensaje de error
-  } else {
-    // Si el campo no está vacío, procede a buscar el clima
-    event.preventDefault(); // Evita el comportamiento por defecto del formulario
-
-    const location = inputLocation.value.trim(); // Elimina espacios en blanco al inicio y al final
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}&units=metric&lang=es`; // Construye la URL de la API con la ubicación ingresada y la clave de API
-
-    actualitationData(url); // Llama a la función para actualizar los datos del clima
-    inputLocation.value = ""; // Limpia el campo de entrada después de la búsqueda
-    console.log("URL de la API:", url);
-    errorContainer.style.display = "none"; // Oculta el contenedor de errores si la búsqueda es exitosa
+    // LLamar las funciones para actualizar el DOM y el video de fondo
+    updateWeatherDisplay(data); // Actualiza los elementos del DOM con los datos obtenidos
+    updateBackgroundVideo(data.weather[0].main); // Cambia el video de fondo según el tipo de clima
+  } catch (error) {
+    console.error("Error al obtener el clima:", error);
   }
 }
 
@@ -190,70 +183,59 @@ function geoLocation() {
     },
     (error) => {
       console.error("No se pudo obtener la ubicación:", error);
-      errorContainer.style.display = "flex"; // Muestra el contenedor de errores
-      errorMessage.textContent =
-        " No se pudo detectar tu ubicación. Por favor, ingresa una ciudad manualmente."; // Muestra un mensaje de error
+      showError(
+        "No se pudo detectar tu ubicación. Por favor, Verifica tu configuracion o ingresa una ciudad manualmente."
+      ); // Muestra un mensaje de error si no se puede obtener la ubicación
     }
   );
 }
 
-// Función para actualizar los datos del clima
-// Esta función toma una URL como parámetro para realizar la solicitud a la API
-async function actualitationData(url) {
-  // Realiza la solicitud a la API de OpenWeatherMap
-  try {
-    const response = await fetch(url);
-    if (!response.ok) {
-      errorContainer.style.display = "flex"; // Muestra el contenedor de errores
-      if (response.status === 401) {
-        errorMessage.textContent =
-          " Hubo un error al conectar con la API, contacta con un administrador de la web"; // Muestra un mensaje de error específico
-      } else {
-        errorMessage.textContent =
-          " No se pudo obtener el clima. Por favor, verifica la ciudad ingresada."; // Muestra un mensaje de error
-      }
-      return; // Sale de la función si la respuesta no es exitosa
-    }
+// Función para buscar el clima
+function searchWeather(event) {
+  event.preventDefault(); // Evita el comportamiento por defecto del formulario
+  if (inputLocation.value === "") {
+    // Verifica si el campo de entrada está vacío
+    showError("Por favor, ingresa una ciudad."); // Muestra un mensaje de error si el campo está vacío
+  } else {
+    // Si el campo no está vacío, procede a buscar el clima
+    event.preventDefault(); // Evita el comportamiento por defecto del formulario
 
-    const data = await response.json(); // Procesa la respuesta JSON de la API
+    const location = inputLocation.value.trim(); // Elimina espacios en blanco al inicio y al final
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}&units=metric&lang=es`; // Construye la URL de la API con la ubicación ingresada y la clave de API
 
-    // Procesa la respuesta JSON de la API
-    console.log("Datos del clima:", data);
-
-    // Actualizar los elementos del DOM con los datos obtenidos
-    locationDisplay.textContent = `${data.name}, ${data.sys.country}`; // Mostrar la ubicación
-
-    temperatureDisplay.textContent = Math.round(data.main.temp) + " °C"; // Mostrar la temperatura
-
-    temperatureRec.textContent = `Max: ${Math.round(
-      data.main.temp_max
-    )} °C, Min: ${Math.round(data.main.temp_min)} °C`; // Mostrar temperatura máxima y mínima
-    pronosticDisplay.textContent = `Pronostico: ${
-      descriptionMap[data.weather[0].main]
-    }`; // Mostrar el pronóstico del clima según el tipo de clima
-
-    humidityDisplay.textContent = `Humedad: ${data.main.humidity}%`; // Mostrar la humedad
-    windDisplay.textContent = `Viento: ${data.dt} m/s`; // Mostrar la velocidad del viento
-    console.log(backgroundMap[data.weather[0].main]);
-
-    // Cambiar el video de fondo según el clima
-    sourceBackground.src =
-      backgroundMap[data.weather[0].main] || "backgrounds/default.mp4"; // Cambiar el video de fondo según el clima
-    videoBackground.load(); // Cargar el nuevo video de fondo
-
-    // Actualizar el icono del clima y su color
-    iconDisplay.className =
-      iconMap[data.weather[0].main] || "fas fa-question-circle";
-    iconDisplay.style.color =
-      data.weather[0].main === "Clear" ? "yellow" : "gray";
-
-    console.log(`Temperatura en ${location}: ${data.main.temp}°C`);
-    console.log(`Clima: ${data.weather[0].main}`);
-
-    const timestamp = data.dt * 1000; // multiplicamos por 1000 para ms
-    const fecha = new Date(timestamp);
-    console.log(fecha.toLocaleString());
-  } catch (error) {
-    console.error("Error al obtener el clima:", error);
+    actualitationData(url); // Llama a la función para actualizar los datos del clima
+    inputLocation.value = ""; // Limpia el campo de entrada después de la búsqueda
+    console.log("URL de la API:", url);
+    errorContainer.style.display = "none"; // Oculta el contenedor de errores si la búsqueda es exitosa
   }
+}
+
+function updateWeatherDisplay(data) {
+  // Actualizar los elementos del DOM con los datos obtenidos
+  locationDisplay.textContent = `${data.name}, ${data.sys.country}`; // Mostrar la ubicación
+
+  temperatureDisplay.textContent = Math.round(data.main.temp) + ""; // Mostrar la temperatura
+
+  temperatureRec.textContent = `Max: ${Math.round(
+    data.main.temp_max
+  )} °C, Min: ${Math.round(data.main.temp_min)} °C`; // Mostrar temperatura máxima y mínima
+
+  pronosticDisplay.textContent = `Pronostico: ${
+    descriptionMap[data.weather[0].main]
+  }`; // Mostrar el pronóstico del clima según el tipo de clima
+
+  humidityDisplay.textContent = `Humedad: ${data.main.humidity}%`; // Mostrar la humedad
+
+  windDisplay.textContent = `Viento: ${data.wind.speed} m/s`; // Mostrar la velocidad del viento
+}
+
+function updateBackgroundVideo(weatherType) {
+  // Cambiar el video de fondo según el tipo de clima
+  sourceIcon.src = iconMap[weatherType] || "backgrounds/weather.svg"; // Cambiar el video de fondo según el clima
+}
+
+function showError(message) {
+  // Mostrar un mensaje de error en el contenedor de errores
+  errorContainer.style.display = "flex"; // Muestra el contenedor de errores
+  errorMessage.textContent = message; // Actualiza el mensaje de error
 }
